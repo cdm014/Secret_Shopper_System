2918246edd4110118e18f81e78b637319657d8ea
cdm014 <chester@rpl.org>
2013-09-30 15:32:30 -0500

    0.3.0-devel.0.1.0+20130930.b
    Added form hander for the reference review and updated the thank you
    page.
    
    Now all that's left is letting them review the reference reviews on the
    back-end

--- application/controllers/review.php
+++ application/controllers/review.php
@@ -1,3 +1,4 @@
+
 <?php
 class Review extends CI_Controller {
 	
@@ -36,11 +37,42 @@ class Review extends CI_Controller {
 		$this->load->view('shopper/thankyou');
 	}
 	
-	public function refSubmit()
+	public function ref_submit()
 	{
 		/*
 			//Stub for submit function for reference review
 		//*/
+		
+		$data = array();
+		$ss_id = $this->input->post('ss_id');
+		$date = $this->input->post('date');
+		$time = $this->input->post('time');
+		
+		$this->load->model('question/question_model','question');
+		
+		$questions = $this->question->get_quiz_question(2);
+		
+		foreach ($questions as $question)
+		{
+			if ($question['question_code'] != "date" && $question['question_code'] != "time"){
+				$value = $this->input->post($question['question_code']);
+				//*
+				$data[] = array(
+					'ss_id' => $ss_id,
+					'date' => $date,
+					'time' => $time,
+					'quiz' => 2,
+					'question' => $question['question_code'],
+					'answer' => $value
+				);
+			}
+			//*/
+		}
+		//*
+		$this->db->insert_batch('ref_reviews',$data);
+		//*/
+		$this->load->helper('url');
+		$this->load->view('shopper/thankyou');
 	}
 	
 	
--- application/views/shopper/thankyou.php
+++ application/views/shopper/thankyou.php
@@ -5,6 +5,6 @@
 <div class="mainBody">
 <h1>Thank You!</h1>
 <p>Thank you for submitting your review. You may either submit a review of another branch or logout of the system.</p>
-<?=anchor('shopper/review_form','Submit Another Review');?> <?=anchor('shopper/logout','Log out of secret shopper system');?>
+<?=anchor('shopper/select_form','Submit Another Review');?> <?=anchor('shopper/logout','Log out of secret shopper system');?>
 </body>
 </html>
