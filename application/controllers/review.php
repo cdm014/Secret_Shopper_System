aac3faf2674dc5b617c560824c2c9fefeec4a8b2
cdm014 <chester@rpl.org>
2013-11-06 12:01:39 -0600

    0.3.0-devel.0.2.0+20131106.b
    added form handler for circulation reviews

--- application/controllers/review.php
+++ application/controllers/review.php
@@ -74,6 +74,44 @@ class Review extends CI_Controller {
 		$this->load->helper('url');
 		$this->load->view('shopper/thankyou');
 	}
+	public function circ_submit()
+	{
+		/*
+			//Stub for submit function for reference review
+		//*/
+		
+		$data = array();
+		$ss_id = $this->input->post('ss_id');
+		$date = $this->input->post('date');
+		$time = $this->input->post('time');
+		
+		$this->load->model('question/question_model','question');
+		
+		$questions = $this->question->get_quiz_question(3);
+		
+		foreach ($questions as $question)
+		{
+			if ($question['question_code'] != "date" && $question['question_code'] != "time"){
+				$value = $this->input->post($question['question_code']);
+				//*
+				$data[] = array(
+					'ss_id' => $ss_id,
+					'date' => $date,
+					'time' => $time,
+					'quiz' => 3,
+					'question' => $question['question_code'],
+					'answer' => $value
+				);
+			}
+			//*/
+		}
+		//*
+		$this->db->insert_batch('circ_reviews',$data);
+		//*/
+		$this->load->helper('url');
+		$this->load->view('shopper/thankyou');
+	
+	}
 	
 	
 }
